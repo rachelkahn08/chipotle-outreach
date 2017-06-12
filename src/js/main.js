39276a4d7f11ebebf18322ed116dbe96f87c197c
Rachel K <rkahn08@gmail.com>
2017-06-12 06:26:59 -0400

    pretty much finished it

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,35 @@
-// set an event listener for ALL horizontal scrolls
-
-// use an if statement so that if the horizontal scroll happens over the dom element, it triggers the function to change the position of that element
-
-// additional if statement so that if the user scrolls or swipes right, it moves right and left if lefts
-
-
-
-
-
-// user interactions: single page
-// click one of the top options and it moves the story to the top and changes the slider
-
-
-
-//---------------------ACTUAL PLAN---------------------
-// on pageload set event listeners to each button
-// write the function to move the object on click
-// if/else statements to target the movement to their own section
-
 function pageLoadListeners() {
 	var allButtons = document.getElementsByClassName('story-button');
 	for (var i = 0; i < allButtons.length; i++) {
-		if (allButtons[i].classList.contains('previous-button')) {
+		if (allButtons[i].classList.contains('previous-button') && !allButtons[i].classList.contains('skip-backward')) {
 			allButtons[i].addEventListener('click', showPreviousSlide);
-		} else if (allButtons[i].classList.contains('next-button')) {
+		} else if (allButtons[i].classList.contains('next-button') && !allButtons[i].classList.contains('skip-forward')) {
 			allButtons[i].addEventListener('click', showNextSlide);
+		} else if (allButtons[i].classList.contains('skip-forward')) {
+			allButtons[i].addEventListener('click', skipToLastSlide);
+		} else if (allButtons[i].classList.contains('skip-backward')) {
+			allButtons[i].addEventListener('click', skipToFirstSlide);
 		}
+	}
+	var hamburgerButton = document.getElementById('hamburgerButton');
+		hamburgerButton.addEventListener('click', toggleMobileMenu);
+
+	window.addEventListener('resize', checkForResize);
+}
+
+function toggleMobileMenu(e) {
+	var mobileMenu = document.getElementById('navBarMobile');
+
+	if (!mobileMenu.classList.contains('hidden')) {
+		mobileMenu.classList.add('hidden');
+		setTimeout(function() {
+			$(mobileMenu).toggle();
+		}, 200);
+	} else {
+		$(mobileMenu).toggle();
+		setTimeout(function() {
+			mobileMenu.classList.remove('hidden');
+		}, 200);
 	}
 }
 
@@ -33,9 +37,7 @@ function createSlideshowAttributes() {
 	var allSlideshows = document.getElementsByClassName('story-container');
 
 	for (var i = 0; i < allSlideshows.length; i++) {
-		
 		var numberOfSlides = allSlideshows[i].children.length;
-		console.log(numberOfSlides);
 		allSlideshows[i].setAttribute("data-number-of-slides", numberOfSlides);
 		allSlideshows[i].setAttribute("data-current-slide", 0);
 	}
@@ -44,73 +46,126 @@ function createSlideshowAttributes() {
 pageLoadListeners();
 
 function showPreviousSlide(e) {
-	var storyToMove = this.parentElement.children[0];
+	var storyToMove = this.parentElement.parentElement.parentElement.children[0];
 	var currentSlide = storyToMove.dataset.currentSlide;
 		currentSlide = currentSlide - 0;
-		currentSlide--;
-		console.log(currentSlide);
 
 	var slideshowLength = storyToMove.dataset.numberOfSlides;
 		slideshowLength = slideshowLength - 0;
+	
+	if (currentSlide > 0) {
+		currentSlide--;
 		
-	storyToMove.setAttribute("data-current-slide", currentSlide);
+		storyToMove.setAttribute("data-current-slide", currentSlide);
+		
+		var nextButtons = this.parentElement.parentElement.children[1];
 
-	var nextButton = this.parentElement.children[2];
+		if (currentSlide === 0) {
+			this.parentElement.classList.remove('visible');
+		} 
 
+		if (currentSlide < slideshowLength - 1) {
+			nextButtons.classList.add('visible');
+		}
 
-	if (currentSlide === 0) {
-		this.style.display = 'none';		
-	} 
-
-	if (currentSlide < slideshowLength) {
-		nextButton.style.display = 'inline';
+		changeSlide(storyToMove);
 	}
 }
 
 function showNextSlide(e) {
-	
-	var storyToMove = this.parentElement.children[0];
+	var storyToMove = this.parentElement.parentElement.parentElement.children[0];
 	var currentSlide = storyToMove.dataset.currentSlide;
 		currentSlide = currentSlide - 0;
-		currentSlide++;
 
 	var slideshowLength = storyToMove.dataset.numberOfSlides;
-		slideshowLength = slideshowLength - 0;
+		slideshowLength = slideshowLength - 1;
 		
-	storyToMove.setAttribute('data-current-slide', currentSlide);
+	if (currentSlide < slideshowLength) {
+		currentSlide++;
 
-	var previousButton = storyToMove.parentElement.childNodes[3];
+		storyToMove.setAttribute('data-current-slide', currentSlide);
+		
+		var previousButtons = this.parentElement.parentElement.children[0];
 
-	if (currentSlide == (slideshowLength - 1)) {
-		this.style.display = 'none';		
-	} 
+		if (currentSlide == (slideshowLength)) {
+			this.parentElement.classList.remove('visible');
+		} 
 
-	if (currentSlide > 0) {
-		previousButton.style.display = 'initial';
+		if (currentSlide > 0) {
+			previousButtons.classList.add('visible');
+		}
+
+		changeSlide(storyToMove);
 	}
+}
+
+function skipToLastSlide(e) {
+	var storyToMove = this.parentElement.parentElement.parentElement.children[0];
+	var slideshowLength = storyToMove.dataset.numberOfSlides;
+		slideshowLength = slideshowLength - 1;
+
+	storyToMove.setAttribute('data-current-slide', slideshowLength);
+
+	this.parentElement.classList.remove('visible');
+	
+	var previousButtons = this.parentElement.parentElement.children[0];
+	if (!previousButtons.classList.contains('visible')) {
+		previousButtons.classList.add('visible');
+	}
+
+	changeSlide(storyToMove);
+}
+
+function skipToFirstSlide(e) {
+	var storyToMove = this.parentElement.parentElement.parentElement.children[0];
+	storyToMove.setAttribute('data-current-slide', '0');
+
+	this.parentElement.classList.remove('visible');
+
+	var nextButtons = this.parentElement.parentElement.children[1];
+	if (!nextButtons.classList.contains('visible')) {
+		nextButtons.classList.add('visible');
+	}
+
+	changeSlide(storyToMove);
 }
 
 function changeSlide(e) {
 	var viewportWidth = document.documentElement.clientWidth;
-	var numberOfSlides = e.dataset.numberOfSlides;
 	var currentSlide = e.dataset.currentSlide;
 	var newLeftValue = -1 * (viewportWidth * currentSlide);
 	e.style.left = newLeftValue;
 }
 
-//-------------reposition on page resize-------------
-// $(window).resize(resizePlaceholder);
+$(window).ready(function() {
+	createSlideshowAttributes();
+});
+
+var triggerResize;
+var resizeCounter = 0;
+var repositionCounter = 0;
+
+function checkForResize() {
+	clearTimeout(triggerResize);
+	triggerResize = setTimeout(repositionSlideshows, 10);
+}
 
 function repositionSlideshows() {
 	var allSlideshows = document.getElementsByClassName('story-container');
-
 	for (var i = 0; i < allSlideshows.length; i++) {
 		changeSlide(allSlideshows[i]);
 	}
 }
 
-$(window).ready(function() {
-	createSlideshowAttributes();
-});
+
+
+
+
+
+
+
+
+
+
 
 
